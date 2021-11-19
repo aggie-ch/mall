0c649e1d5db7223c5c3f5dcedbeac7242988d627
aggie-ch <aga.chorowiec@gmail.com>
2021-11-19 18:53:48 +0100

    feat: modified components for frontend

--- mall-ui/src/app/app-routing.module.ts
+++ mall-ui/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import {UsersListComponent} from "./component/users-list/users-list.component";
+import {UserFormComponent} from "./component/user-form/user-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersListComponent
+  },
+  {
+    path: 'add-user',
+    component: UserFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mall-ui/src/app/app.module.ts
+++ mall-ui/src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {Api} from "./api";
+import { UsersListComponent } from './component/users-list/users-list.component';
+import {UserService} from "./service/user.service";
+import { UserFormComponent } from './component/user-form/user-form.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    Api,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
